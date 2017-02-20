2baf0c0ddd9eb857e927555b221b35d11b6bb8db
Tony Powell <adpowell@mtu.edu>
2017-02-20 13:08:29 -0500

    Demonstrative branding changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Iditarod!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
-        </p>
+		<div className="app-intro">
+			<a className="waves-effect waves-light btn" href="#">Login!</a>
+		</div>
       </div>
     );
   }
