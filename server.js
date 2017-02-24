6006d858f928cef734afa660ead366df553aeab8
Tony Powell <adpowell@mtu.edu>
2017-02-24 11:31:42 -0500

    Initialized firebase connectivity.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const firebase = require('firebase-admin');
 
+// Configure Firebase connection information
+// Initialize Firebase
+const serviceAccount = require('./firebase-credentials.json');
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://iditarod-b06d6.firebaseio.com"
+});
+
+// Configure express to listen on localhost, deliver the latest React Build
 app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
@@ -10,3 +20,4 @@ app.get('/', function (req, res) {
 });
 
 app.listen(9000);
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './style/Login.css';
 
 class Login extends Component {
   render() {
     return (
 		<div>
+			<div className='intro'>
+				<h1 className="">Iditarod</h1>
+				<h2 className="">MTU Course Planner</h2>
+			</div>
 			Login Component!
 		</div>
     );
