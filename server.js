9c249df661729d12d1d7c4e03883b995c50bc649
Tony Powell <adpowell@mtu.edu>
2017-03-13 10:57:29 -0400

    Code commented.

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ firebase.initializeApp({
 });
 const database = firebase.database();
 
-// Configure express to listen on localhost, deliver the latest React Build
+// Configure express to serve files from the build folder
 app.use(express.static('./build'));
 
-app.get('*', function (req, res) {
-	
-	// Any url that is not an endpoint is just ignored
+// Intercept all URLs, switch on URL
+app.get('*', function (req, res) {	
 	// Create endpoints as functions and call them based on the url
 	switch (req.path) {
 	case '/courses/allCourses':
 		getAllCourses(req, res);
 		break;
 
+// Just deliver the index if the url is not a data endpoint
 	default:
 		res.sendFile(path.join(__dirname, './build', 'index.html'));
 		break;
@@ -34,8 +34,9 @@ app.get('*', function (req, res) {
 
 // Endpoints for client retrieval of data
 let getAllCourses = function (req, res) {
-	console.log('Request sent for course data...');
-	// Get all courses
+	console.log('Requesting course data from Firebase...');
+
+	// Get all courses from firebase
 	let courses = database.ref('Courses');
 	courses.on('value', function (snapshot) {
 		res.send({
