b4822efccad2a502266ca0549fc3b778099a89f1
bmwichma <bmwichma@mtu.edu>
2017-03-17 12:02:15 -0400

    Adjust login page with new login skeleton

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './style/Login.css';
 import GoogleLogin from 'react-google-login';
+//import GoogleAuth from 'vue-google-auth';
 
 class Login extends Component {
 	constructor() {
@@ -14,8 +15,13 @@ class Login extends Component {
 			this.context.router.transitionTo('/dashboard');
 		}
 	}
-	/*//Code for the sign out function. Will work once gapi is figured out
-	window.onLoadCallback = function signOut(googleUser) {
+
+	// Code for possible future signin/out
+	/*Vue.use(GoogleAuth, { clientID: '419580728333-lrjvoak9e2kjapl3b6t82nlekrkl3ltq.apps.googleusercontent.com'})
+	Vue.googleAuth().load()*/
+
+	//Code for the sign out function. Will work once gapi is figured out
+	/*signOut(googleUser) {
 		var auth2 = gapi.auth2.getAuthInstance();
 		auth2.signOut().then(function () {
 		console.log('User signed out.');
@@ -30,7 +36,7 @@ class Login extends Component {
 					<h2>MTU Course Planner</h2>
 				</div>
 				<div className='content-section'>
-					<div className='valign-wrapper'>
+					<div className='intro'>
 						<GoogleLogin
 							clientId="419580728333-lrjvoak9e2kjapl3b6t82nlekrkl3ltq.apps.googleusercontent.com"
 							buttonText="Login with MTU Email"
