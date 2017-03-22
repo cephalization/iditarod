7c2973a0749caa42ef3cd42dcca89a3e1f56736c
bmwichma <bmwichma@mtu.edu>
2017-03-22 12:17:55 -0400

    Moved courses from dashboard to course page

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
+import * as FirebaseActions from '../firebaseFunctions';
 
 class Course extends Component {
+
+	constructor() {
+		// Required function call for every constructor
+		super();
+
+		// Bind every class function to 'this'
+		this.retrieveCourses = this
+		.retrieveCourses
+		.bind(this);
+		//Set up our state
+		this.state = {
+			courses: []
+		};
+		//Async call to retrieve courses
+		this.retrieveCourses();
+	}
 
 	componentWillMount(){
 		this.props.checkAuth("Courses");
 	}
 
+
+	retrieveCourses() {
+		// Make modifications to an object referring the class's 'this'
+		let coursesRef = this;
+		// Fetch the data from firebase
+		FirebaseActions.allCourses(function (response) {
+			let courses = [];
+
+			for (let course in response.courses) {
+				if (response.courses.hasOwnProperty(course)) {
+					//console.log('course is', course);
+					courses.push(
+						// This code needs to be abstracted into a class Component Talk to Tony about
+						// how to pass props, etc
+						<li key={course}> { course + ' - ' + response.courses[course].name}</li>
+					);
+				}
+			}
+			coursesRef.setState({
+				courses: courses
+			});
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
-				<div className="container">
-					<h3>Courses Component!</h3>
+				<div className='class'>
+					<h1>Course List</h1>
+					<ul>
+						{this.state.courses}
+					</ul>
 				</div>
 			</div>
 		);
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,17 +5,6 @@ class Dashboard extends Component {
 	constructor() {
 		// Required function call for every constructor
 		super();
-
-		// Bind every class function to 'this'
-		this.retrieveCourses = this
-		.retrieveCourses
-		.bind(this);
-		//Set up our state
-		this.state = {
-			courses: []
-		};
-		//Async call to retrieve courses
-		this.retrieveCourses();
 	}
 
 	componentWillMount(){
@@ -23,37 +12,12 @@ class Dashboard extends Component {
 		this.props.checkAuth("Dashboard");
 	}
 
-	retrieveCourses() {
-		// Make modifications to an object referring the class's 'this'
-		let coursesRef = this;
-		// Fetch the data from firebase
-		FirebaseActions.allCourses(function (response) {
-			let courses = [];
-
-			for (let course in response.courses) {
-				if (response.courses.hasOwnProperty(course)) {
-					//console.log('course is', course);
-					courses.push(
-						// This code needs to be abstracted into a class Component Talk to Tony about
-						// how to pass props, etc
-						<li key={course}> { course + ' - ' + response.courses[course].name}</li>
-					);
-				}
-			}
-			coursesRef.setState({
-				courses: courses
-			});
-		});
-	}
-
 	render() {
 
 		return (
 			<div>
 				<div className="container">
-					<ul>
-						{this.state.courses}
-					</ul>
+					<h2>Dashboard</h2>
 				</div>
 			</div>);
 	}
