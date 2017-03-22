ca6223dab86dd2a71813567bb862ab860441e74d
Alex David <ajdavid@mtu.edu>
2017-03-22 11:15:41 -0400

    Cleaned up script to post courses to FireBase
    - Removed commented out sections and blank lines

--- parsing/postData.js
+++ parsing/postData.js
@@ -1,19 +1,12 @@
 postData();
 
-
-//process.exit();
-
-
-
 function postData() {
 	let parser = require('./parser.js');
 
 	parser.getData().then(function(data) {
 
 		let admin = require('firebase-admin');
-
 		let serviceAccount = require('../firebase-credentials.json');
-
 		let app = admin.initializeApp({
 			credential: admin.credential.cert(serviceAccount),
 			databaseURL: 'https://iditarod-b06d6.firebaseio.com'
@@ -26,12 +19,6 @@ function postData() {
 				console.log('it ran');
 				app.delete();
 			});
-
-		//console.log(data);
-
-		console.log('posted data');
-		//app.delete();
-
 	});
 
 	return;
