3ed519ab2ccc04ef50be9adf61e10d4d4844cc0a
Tony Powell <adpowell@mtu.edu>
2017-04-07 11:50:07 -0400

    Described the audit generation code flow

--- server.js
+++ server.js
@@ -29,11 +29,33 @@ app.get('*', function (req, res) {
 app.post('*', function(req, res){
 	console.log('Post request caught for', req.path);
 	switch(req.path){
+	case '/run/audit':
+		generateAudit(req);
+		break;
 	default:
 		res.sendFile(path.join(__dirname, './build', 'index.html'));
 		break;
 	}
 });
+
+// Request contains
+// .uid = user's uid on the database
+// .type = degree to run audit on
+// .requirementsMet = object that contains fulfilled HASS and select_from
+function generateAudit(request) {
+
+	// Loading relevant user information from firebase
+	// User's taken courses, credits taken
+
+	// Load degree audit requirements from firebases
+
+	// Create a user audit object
+	// it will have a property for each requirement
+	// each requirement will have a property for courses taken fulfilling this property, courses yet to be taken, credits completed for requirement
+	// Parse degree audit per pseudo specs, save results in user audit object
+
+	// Save a completed audit to the database
+}
 
 // Endpoints for client retrieval of data
 
