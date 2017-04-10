8b756b5c4b199a90228fd5a0e1f2686ac47d1821
Alex David <ajdavid@mtu.edu>
2017-04-10 11:24:18 -0400

    Created empty function to compare courses, audit
    - Added an empty function that will compare a user's courses and a
    degree audit, returning requirements completed and uncompleted

--- auditLogic.js
+++ auditLogic.js
@@ -1,3 +1,17 @@
 /* Function(s) for comparing user's courses with degree requirements
  *
  */
+
+/* Compares a user's taken courses with a degree's requirements and returns an
+ * object containing lists of classes towards the degree and classes still
+ * needed.
+ *
+ * @param courses An object containing all of courses a user has taken.
+ * @param audit An object containing all the requirements of a degree.
+ * @return An object containing a list of courses that meet degree requirements
+ * and a list of requirements not known to have been met.
+ */
+function compareCoursesToAudit(courses, audit)
+{
+
+}
